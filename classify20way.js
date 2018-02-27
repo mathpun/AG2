62f65859399af901d2cd3782d214a0db16d9122e
mathpun <eulersmail@gmail.com>
2018-02-27 18:30:12 -0500

    changed nway = 3
    was 4!!

--- classify20way.js
+++ classify20way.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 	// Parameters
 	var ntask = 12; // how many different tasks (alphabets) are there?
-	var nway = 4; // n-way classification tasl
+	var nway = 3; // n-way classification tasl
 
 	// Selected demo images
 	//latin_id = 2;
